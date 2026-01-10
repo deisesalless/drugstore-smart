b6ec54c6109b749ff26dbf4912f90a1ea911722f
deisesalless <deisesalles18@gmail.com>
2026-01-09 22:07:50 -0300

    feat(entity): add medicine

---
+++ src/main/java/com/drugstore/smart/entity/Medicine.java
@@ -0,0 +1,33 @@
+package com.drugstore.smart.entity;
+
+import com.drugstore.smart.enums.Manufacturer;
+import com.drugstore.smart.enums.MedicineForm;
+import com.drugstore.smart.persistence.converter.ManufacturerConverter;
+import com.drugstore.smart.persistence.converter.MedicineFormConverter;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table
+public class Medicine {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String dosage;
+
+    @Convert(converter = MedicineFormConverter.class)
+    private MedicineForm form;
+
+    @Convert(converter = ManufacturerConverter.class)
+    private Manufacturer manufacturer;
+
+    private LocalDate expirationDate;
+    private BigDecimal price;
+    private Integer quantityInStock;
+    private String description;
+}
