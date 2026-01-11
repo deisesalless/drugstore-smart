d510c0cc53a9fb55b6dba5dfbbb969f7e9f7b89f
deisesalless <deisesalles18@gmail.com>
2026-01-11 00:18:47 -0300

    fix(mapper): ignored id

--- src/main/java/com/drugstore/smart/mapper/MedicineMapper.java
+++ src/main/java/com/drugstore/smart/mapper/MedicineMapper.java
@@ -1,4 +1,19 @@
 package com.drugstore.smart.mapper;
 
+import com.drugstore.smart.dto.MedicineCreateDTO;
+import com.drugstore.smart.dto.MedicineDTO;
+import com.drugstore.smart.entity.Medicine;
+import com.drugstore.smart.enums.Manufacturer;
+import com.drugstore.smart.enums.MedicineForm;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface MedicineMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Medicine toEntity(MedicineCreateDTO dto);
+    MedicineDTO toDTO(Medicine entity);
 }
