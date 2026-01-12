eb327eeead038c4ea4a455e88cc497f99a8f567c
deisesalless <deisesalles18@gmail.com>
2026-01-12 14:52:00 -0300

    config(MedicineCreateDTO): add validation messages

--- src/main/java/com/drugstore/smart/dto/MedicineCreateDTO.java
+++ src/main/java/com/drugstore/smart/dto/MedicineCreateDTO.java
@@ -2,18 +2,20 @@ package com.drugstore.smart.dto;
 
 import com.drugstore.smart.enums.Manufacturer;
 import com.drugstore.smart.enums.MedicineForm;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record MedicineCreateDTO(
-    String name,
-    String dosage,
-    MedicineForm form,
-    Manufacturer manufacturer,
-    LocalDate expirationDate,
-    BigDecimal price,
-    Integer quantityInStock,
-    String description
+    @NotBlank(message = "Name is required") String name,
+    @NotBlank(message = "Dosage is required") String dosage,
+    @Enumerated MedicineForm form,
+    @Enumerated Manufacturer manufacturer,
+    @Future(message = "Expiration date must be in the future") LocalDate expirationDate,
+    @NotNull(message = "Price is required") BigDecimal price,
+    @Min(value = 1, message = "Minimium quantity is 1") Integer quantityInStock,
+    @NotBlank(message = "Description is required") String description
 ) {
 }
